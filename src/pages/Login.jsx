61a102bc75b8471d78a68fc740064138a43a5e2d
Yiseul Kwon <yiseul@Yiseuls-Mac-mini.local>
2025-04-06 20:56:27 +0900

    로그인 필드 required 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,10 +12,10 @@ const Login = ({setAuthenticate}) => {
   }
   return (
     <Container>
-      <Form onSubmit={submitLogin}>
+      <Form onSubmit={submitLogin} className='form-login'>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="Enter email" required />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -23,7 +23,7 @@ const Login = ({setAuthenticate}) => {
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" required/>
         </Form.Group>
         
         <Button variant="danger" type="submit">
